6fe329de241f63fcdf3f33d010df391d75bfbc32
imamiya-masaki <imamiyagit@gmail.com>
2025-03-23 04:50:14 +0900

    iconの不調を戻す

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -1,52 +1,44 @@
 import path from 'node:path';
 import webpack from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import TerserPlugin from 'terser-webpack-plugin'
+
 console.log('webpack',  process?.env?.["NODE_ENV"])
 /**
  * NODE_ENV が 'development' なら true、そうでなければ false
  */
 const isDev = process?.env?.["NODE_ENV"] === 'development';
 const isAnalyze = process?.env?.["NODE_ENV"] === 'analyze';
-
-const { default: UnoCSS } = await import('@unocss/webpack');
-
 /** @type {import('webpack').Configuration} */
 const config = {
   // mode と devtool を分岐
-  // @ts-ignore
-  mode: (process?.env?.["NODE_ENV"] ) ?? 'production',
+  mode: isDev ? 'development' : 'production',
   devtool: isDev ? 'inline-source-map' : false,
-  cache: false,
+
   entry: './src/main.tsx',
   module: {
     rules: [
       {
         exclude: [/node_modules\/video\.js/, /node_modules\/@videojs/],
-        // resolve: {
-        //   fullySpecified: false,
-        // },
+        resolve: {
+          fullySpecified: false,
+        },
         test: /\.(?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
-              // [
-              //   '@babel/preset-env',
-              //   {
-              //     // モダンブラウザ向けにターゲットを限定
-              //     targets: ['last 2 Chrome versions'],
-              //   },
-              // ],
+              [
+                '@babel/preset-env',
+                {
+                  // モダンブラウザ向けにターゲットを限定
+                  targets: ['last 2 Chrome versions'],
+                },
+              ],
               ['@babel/preset-react', { runtime: 'automatic' }],
               ['@babel/preset-typescript'],
             ],
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.png$/,
@@ -78,35 +70,14 @@ const config = {
     outputModule: true, // ESM出力を有効化
   },
   plugins: [
-    // new UnoCSSWebpackPlugin({
-    //   // @ts-expect-error
-    //   configOrPath: path.resolve(import.meta.dirname, './uno.config.js'),
-    // }),
-        new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: isDev ? "production" : "development" }),
-    // UnoCSS(),
+    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: isDev ? "production" : "development" }),
     // 開発環境のときだけ BundleAnalyzerPlugin を追加
     ...(isAnalyze ? [new BundleAnalyzerPlugin()] : []),
   ],
 
-  snapshot: {
-    immutablePaths: [/uno./]
-  },
   resolve: {
     extensions: ['.js', '.cjs', '.mjs', '.ts', '.cts', '.mts', '.tsx', '.jsx'],
   },
 };
 
-export default function () {
-  return import('@unocss/webpack').then(() => (
-
-    {
-    plugins: [
-      // UnoCSS(),
-      ...config.plugins ?? [],
-    ],
-    optimization: {
-      realContentHash: true,
-    },
-    ...config,
-  }))
-};
+export default config;
